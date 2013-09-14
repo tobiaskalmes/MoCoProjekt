fba4efe8fafa35d55544b07f55edc457ba15fe03
Tobias <tobiaskalmes@gmail.com>
2013-09-14 14:47:00 +0200

    restful test

--- MoCo Server/src/de/htw/toto/moco/server/Main.java
+++ MoCo Server/src/de/htw/toto/moco/server/Main.java
@@ -1,5 +1,10 @@
 package de.htw.toto.moco.server;
 
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Tobias
@@ -8,7 +13,16 @@ package de.htw.toto.moco.server;
  * To change this template use File | Settings | File Templates.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServerFactory.create("http://localhost:9998/");
+        server.start();
 
+        System.out.println("Server running");
+        System.out.println("Visit: http://localhost:9998/helloworld");
+        System.out.println("Hit return to stop...");
+        System.in.read();
+        System.out.println("Stopping server");
+        server.stop(0);
+        System.out.println("Server stopped");
     }
 }
--- MoCo Server/src/de/htw/toto/moco/server/communication/Login.java
+++ MoCo Server/src/de/htw/toto/moco/server/communication/Login.java
@@ -2,6 +2,7 @@ package de.htw.toto.moco.server.communication;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,9 +18,10 @@ import javax.ws.rs.core.MediaType;
 public class Login {
 
     @GET
+    @Path(value = "/{username}/{passwordhash}")
     @Produces(MediaType.TEXT_HTML)
-    public String test()
-    {
-        return "<html><body>test</body></html>";
+    public String login(@PathParam(value = "username") String username, @PathParam(value = "passwordhash") String
+            passwordhash) {
+        return "<html><body>username: " + username + "<br/>passwordhash:" + passwordhash + "</body></html>";
     }
 }
