a2e48ef42750ecc674be6cd0e000e3642e7c7e84
Tobias <tobiaskalmes@gmail.com>
2013-09-14 11:54:18 +0200

    init server

---
+++ MoCo Server/src/de/htw/toto/moco/server/Main.java
@@ -0,0 +1,13 @@
+package de.htw.toto.moco.server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tobias
+ * Date: 14.09.13
+ * Time: 11:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
