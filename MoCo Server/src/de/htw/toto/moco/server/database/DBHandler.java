e52eabc42967b702c522ad1473948856d522a708
togu <tobias.gundlach@gmail.com>
2013-09-16 16:34:15 +0200

    anfang vom db backend

--- MoCo Server/src/de/htw/toto/moco/server/database/DBHandler.java
+++ MoCo Server/src/de/htw/toto/moco/server/database/DBHandler.java
@@ -8,4 +8,5 @@ package de.htw.toto.moco.server.database;
  * To change this template use File | Settings | File Templates.
  */
 public class DBHandler {
+
 }
---
+++ MoCo Server/src/de/htw/toto/moco/server/navigation/Poi.java
@@ -0,0 +1,73 @@
+package de.htw.toto.moco.server.navigation;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: TG
+ * Date: 15.09.13
+ * Time: 18:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class Poi {
+    private double latitude, longitude;
+    private String name;
+    private boolean active;
+    private int type;
+    private Integer idPoi;
+
+    public Poi(double latitude, double longitude, String name, boolean active, int type, Integer idPoi) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.name = name;
+        this.active = active;
+        this.type = type;
+        this.idPoi = idPoi;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Integer getIdPoi() {
+        return idPoi;
+    }
+
+    public void setIdPoi(Integer idPoi) {
+        this.idPoi = idPoi;
+    }
+}
