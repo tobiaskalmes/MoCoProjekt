d83da8d2f997719ba0ecab1c8ccf132cfd8332d6
Tobias <tobiaskalmes@gmail.com>
2013-09-22 20:06:08 +0200

    compass is now "funky"

--- MoCo App/src/de/htw/toto/moco/app/gui/views/CompassView.java
+++ MoCo App/src/de/htw/toto/moco/app/gui/views/CompassView.java
@@ -41,26 +41,48 @@ public class CompassView extends View {
         int h = getMeasuredHeight();
         int r;
         if (w > h) {
-            r = (h / 2) - 5;
+            r = (h / 2) - 15;
         } else {
-            r = (w / 2) - 5;
+            r = (w / 2) - 15;
         }
 
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(5);
-        paint.setColor(Color.WHITE);
-
+        paint.setStrokeWidth(15);
+        paint.setColor(Color.rgb(167, 167, 167));
         canvas.drawCircle(w / 2, h / 2, r, paint);
 
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(3);
+        canvas.drawCircle(w / 2, h / 2, r + 6, paint);
+        canvas.drawCircle(w / 2, h / 2, r - 6, paint);
+
+        paint.setStrokeWidth(7);
+        paint.setColor(Color.WHITE);
+        canvas.drawLine(w / 2, h / 2, (float) (w / 2 + (r + 2) * Math.sin(-direction)),
+                        (float) (h / 2 - (r + 2) * Math.cos(-direction)), paint);
+        paint.setStrokeWidth(5);
         paint.setColor(Color.RED);
         canvas.drawLine(w / 2, h / 2, (float) (w / 2 + r * Math.sin(-direction)),
                         (float) (h / 2 - r * Math.cos(-direction)), paint);
         if (bearing != null) {
+            paint.setStrokeWidth(7);
+            paint.setColor(Color.WHITE);
+            canvas.drawLine(w / 2, h / 2,
+                            (float) (w / 2 + (r + 2) * Math.sin(-(direction - (bearing * Math.PI / 180)))),
+                            (float) (h / 2 - (r + 2) * Math.cos(-(direction - (bearing * Math.PI / 180)))), paint);
+            paint.setStrokeWidth(5);
             paint.setColor(Color.GREEN);
             canvas.drawLine(w / 2, h / 2, (float) (w / 2 + r * Math.sin(-(direction - (bearing * Math.PI / 180)))),
                             (float) (h / 2 - r * Math.cos(-(direction - (bearing * Math.PI / 180)))), paint);
         }
+
+        paint.setStrokeWidth(25);
+        paint.setColor(Color.RED);
+        canvas.drawCircle(w / 2, h / 2, 12, paint);
+        paint.setStrokeWidth(3);
+        paint.setColor(Color.WHITE);
+        canvas.drawCircle(w / 2, h / 2, 24, paint);
     }
 
     public void update(float dir) {
